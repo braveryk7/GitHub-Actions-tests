072d3b6dee007347584c5efc4ce7eb77d329c11d
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-06-17 23:54:58 +0900

    add phpcod

--- class/class-example-class.php
+++ class/class-example-class.php
@@ -4,6 +4,9 @@ class Example_Class {
         return update_option( 'my_option', 'my_value' );
     }
 
+    /**
+     * PHPDoc.
+     */
     public function get_my_option(): string {
         return get_option( 'my_option' );
     }
